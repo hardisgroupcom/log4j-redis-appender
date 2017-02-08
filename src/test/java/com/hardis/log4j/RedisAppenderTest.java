22b43a3887ac7c4dac76cc061c5364584af4b68a
cleroy38 <chleroy23@gmail.com>
2017-02-08 17:57:00 +0100

    Improvement of test code

--- src/test/java/com/hardis/log4j/RedisAppenderTest.java
+++ src/test/java/com/hardis/log4j/RedisAppenderTest.java
@@ -1,59 +1,113 @@
-/**
-* This file is part of log4j2redis
-*
-* Copyright (c) 2012 by Pavlo Baron (pb at pbit dot org)
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-
-* @author Pavlo Baron <pb at pbit dot org>
-* @author Landro Silva
-* @copyright 2012 Pavlo Baron */
-
 package com.hardis.log4j;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class RedisAppenderTest {
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
+public class RedisAppenderTest {
+	
+	private Jedis redis;
+	private static String key;	
+	
+	private static int nbThread = 1;
+	private static int nbLogs = 1000;
+	
 	public static class LogThread extends Thread {
+		
 		private static final AtomicInteger count = new AtomicInteger();
+		
 		Logger log = Logger.getLogger("LogThread" + count.incrementAndGet());
+		
 		public void run() {
 			try {
 				for (long i = 0; i < 1000; i++) {
 					log.trace("whatever " + i);
-					Thread.sleep(1000);
+					Thread.sleep(10);
 				}
 			} catch (InterruptedException e) {}
 		}
 	}
 
 	private static final Logger log = Logger.getLogger("LogMainThread");
+	
+	@BeforeClass
+	public static void beforeClass() throws IOException{
+		
+		// Read the redis key		
+		Properties prop = new Properties();
+		InputStream input = null;
 
+		try {
+			ClassLoader loader = Thread.currentThread().getContextClassLoader();
+			input = loader.getResourceAsStream("log4j.properties");		
+			prop.load(input);
+			key = prop.getProperty("log4j.appender.redis.key");
+		} finally {
+			if (input != null) {
+				input.close();
+			}
+		}
+		
+		// Remove the redis key
+		JedisPool pool = null;
+		Jedis jedis = null;
+		try {
+			pool = new JedisPool("localhost");
+			jedis = pool.getResource();
+			jedis.del(key);
+		} finally {
+			if (pool != null){
+				pool.close();	
+			}				
+			if (jedis != null){
+				jedis.close();	
+			}					
+		}
+		
+	}	
+	
+	@Before
+	public void setUp() {		
+		JedisPool pool = new JedisPool("localhost");
+		redis = pool.getResource();
+		// clear the redis list first
+		redis.ltrim(key, 1, 0);
+	}
+	
+	@After
+	public void tearDown() {		
+		if (redis != null){
+			redis.close();
+		}
+	}	
+
+	
 	@Test
-	public void test() throws Throwable {
-		for (int i = 1; i <= 9; i++) {
+	public void test() throws Throwable {		
+		for (int i = 0; i < RedisAppenderTest.nbThread; i++) {
 			new RedisAppenderTest.LogThread().start();
 		}
 
-		for (long i = 0; i < 1000; i++) {
+		for (long i = 0; i < RedisAppenderTest.nbLogs; i++) {
 			log.debug("that's me " + i);
-			Thread.sleep(1000);
+			Thread.sleep(100);
 		}
+		
+		// list length check
+		long len = redis.llen(key);
+		assertEquals(RedisAppenderTest.nbThread*RedisAppenderTest.nbLogs, len);		
 	}
 
 }
